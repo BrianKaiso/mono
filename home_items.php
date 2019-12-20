6425bfe9b521a3213a64378f313b0741db50f2f0
Brian <info@venture-japan.com>
2019-12-20 16:21:47 +0900

    #13 商品登録画面を追加しました。

--- home_items.php
+++ home_items.php
@@ -34,25 +34,26 @@
  }
 
    // 自己紹介文一覧表示
-   $sql ="SELECT * FROM mst_intro WHERE c_code = '{$_SESSION["users_id"]}'";
+   $sql ="SELECT * FROM mst_product WHERE c_code = '{$_SESSION["users_id"]}'";
    $stmt = $pdo->prepare($sql);
    $status = $stmt->execute(); // 成功ならtrue, 失敗ならfalse
- 
+
    $view = '';
    if($status==false) {
      sql_error($stmt); // include -> functions.php > function sql_error();
    }else{
-    while($r2 = $stmt->fetch(PDO::FETCH_ASSOC)){  
-      $view .= "<div class=\"intro\"><div><img src='home/intro/{$_SESSION["users_id"]}/{$r2['media']}' width='100' height='100' /></div><div><p>{$r2['title']}</p><p>{$r2{'text'}}</p></div><div><a href='home/home_intro_delete.php?id={$r2['id']}'>削除</a></div></div>";
-    }  
- }
+     while($r2 = $stmt->fetch(PDO::FETCH_ASSOC)){  
+       $view .= "<div class=\"intro\"><div><img src='home/items/{$_SESSION["users_id"]}/{$r2['p_img']}' width='100' height='100' /></div><div><p>{$r2['p_name']}</p><p>{$r2{'p_spec'}}</p><p>{$r2['p_text']}</p></div><div><a href='home/home_items_delete.php?id={$r2['p_code']}'>削除</a></div></div>";
+     }
+    }
 
-if(empty($view)){
-  // データが無いとき
-  $view ="<p>自己紹介ページ用のコンテンツはまだ投稿されておりません。さっそく情報を登録しましょう！</p>";
-}
+ if(empty($view)){
+    // データが無いとき
+    $view ="<p>商品がまだ1件もありません。さっそく商品を登録しましょう！</p>";
+  }
+  
 
-$pdo=null;
+ $pdo=null;
 // DB接続エンドHere
 
 // JSの処理で使用するのでセッションIDをJSの変数に渡しておく
@@ -61,14 +62,14 @@ $pdo=null;
 ?>
 
 <main> <!-- マイページ コンテンツここから   -->
-<h1>マイページ - 自己紹介</h1>
+<h1>マイページ - 商品登録</h1>
 <?php
 // navigation include
 include(__DIR__.'/include/home/mypagenav.php');  
 ?>
 
 <!-- マイページ基本情報表示と更新  -->
-  <p><?=$r["name"] ?>さんのストーリー、こだわりを教えてください！ここで登録した情報は自己紹介ページで表示されます。</p> <!-- 社名/屋号  -->
+  <p><?=$r["name"] ?>さんの商品をこちらで登録・編集することができます。</p> <!-- 社名/屋号  -->
   <?php
     if(isset($_SESSION["intro_edit_check"])){
       $error="";
@@ -84,13 +85,15 @@ include(__DIR__.'/include/home/mypagenav.php');
    ?>
   <!-- 登録フォームを表示する   -->
   <fieldset>
-    <legend>情報登録</legend>
-    <form method="post" action="home/home_intro_edit.php" enctype="multipart/form-data">
+    <legend>商品登録</legend>
+    <form method="post" action="home/home_item_edit.php" enctype="multipart/form-data">
     <dl>
-      <dt>タイトル<dt>
-      <dd><input type="text" id="title" name="title" size="80" maxLength="100" placeholder="タイトル" /><dd>
-      <dt>本文</dt>
-      <dd><textarea type="textarea" id="textarea" name="textarea" rows="10" cols="100" placeholder="本文" /></textarea></dd>
+      <dt>商品名<dt>
+      <dd><input type="text" id="title" name="title" size="80" maxLength="30" placeholder="タイトル(30文字以内)" /><dd>
+      <dt>商品仕様</dt>
+      <dd><textarea type="textarea" id="spec" name="spec" rows="5" cols="100" placeholder="規格・容量・サイズなどなど" /></textarea></dd>
+      <dt>商品紹介</dt>
+      <dd><textarea type="textarea" id="desc" name="desc" rows="10" cols="100" placeholder="商品の魅力をたっぷりと語ってください！" /></textarea></dd>
       <dt>メディア<dt>
       <dd><input type="file" name="upfile"></dd>
     </dl>
